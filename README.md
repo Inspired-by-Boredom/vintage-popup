d2bff7ff4fc059062093dbf0d15cfba0861eee36
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2017-02-13 22:43:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,379 @@
+# Vintage popup
+
+Vintage popup window plugin
+
+## Installation
+
+Install popup plugin
+
+```
+npm i -S vintage-popup
+```
+
+Add popup to your project
+
+AMD
+
+```javascript
+require(['vintage-popup'], function (Popup) {});
+```
+
+CommonJS
+
+```javascript
+var Popup = require('vintage-popup');
+```
+
+ES6 (Webpack)
+
+```javascript
+import Popup from 'vintage-popup';
+```
+
+Inline
+
+```html
+<script src="vintage-popup.js"></script>
+```
+
+Include CSS file
+
+```html
+<link href="vintage-popup.css" rel="stylesheet">
+<link href="popup-default-theme.css" rel="stylesheet">
+```
+
+## Initialization
+
+### There are several ways to initialize popup module.
+
+```javascript
+// Initialize popup on the elements with 'data-popup-target' attribute.
+Popup.initialize();
+
+// Initialize popup on the elements with 'data-popup-target' attribute
+// and change some options for example.
+Popup.initialize({
+  bodyFixedOnDesktop: false
+});
+
+// Initialize popup on the div elements with 'data-popup-remote' attribute.
+Popup.initialize('div[data-popup-remote]');
+
+// Initialize popup on the elements with 'ex' class.
+// Disable some options for example.
+Popup.initialize('.ex', {
+  closeOnBgClick: false,
+  closeOnEsc: false
+});
+
+// Initialize popup on the elements with 'ex' class.
+// jQuery way.
+$('.ex').popup();
+
+// Initialize popup on the elements with 'remotePopup' class.
+// Add remote data source for example.
+$('.remotePopup').popup({
+  remote: {
+    url: 'ajax/example.json'
+  }
+});
+```
+
+## Examples
+
+## Default popup
+
+```html
+<!-- Button trigger modal -->
+<button type="button" data-popup-target="example">
+  Default popup
+</button>
+
+<!-- Popup -->
+<div class="popup" data-popup-id="example" tabindex="-1" role="dialog">
+    <div class="popup__container">
+        <div class="popup__close"><span></span><span></span></div>
+        <div class="popup__content">
+            <div class="popup__title">Popup title</div>
+            <div class="popup__body">Popup body</div>
+        </div>
+    </div>
+</div>
+```
+
+## Popup with remote data source
+
+```html
+<!-- Button trigger modal -->
+<button type="button" data-popup-target="exampleRemote" data-popup-remote="/path/example.json">
+  Remote popup
+</button>
+
+<!-- Popup -->
+<div class="popup" data-popup-id="exampleRemote" tabindex="-1" role="dialog">
+    <div class="popup__container">
+        <div class="popup__close"><span></span><span></span></div>
+        <div class="popup__content"></div>
+    </div>
+</div>
+```
+
+### example.json
+
+```json
+{
+  "replaces": [
+    {
+      "what": "[data-popup-id='exampleRemote'] .popup__content",
+      "data": "<div class='popup__content'><div class='popup__title'>Popup title</div><div class='popup__body'>Popup body</div></div>"
+    }
+  ]
+}
+```
+
+## Options
+
+### closeOnBgClick
+
+Type: `Boolean`
+
+Default: `true`
+
+If true, closes the popup by clicking anywhere outside it.
+
+### bodyFixedOnDesktop
+
+Type: `Boolean`
+
+Default: `true`
+
+If true, sets position: fixed to the body on desktop.
+
+### closeOnEsc
+
+Type: `Boolean`
+
+Default: `true`
+
+If true, closes the popup after pressing the ESC key.
+
+### eventsNameSpace
+
+Type: `String`
+
+Default: `'popup'`
+
+Attached jQuery events namespace.
+
+### targetPopupId
+
+Type: `String`
+
+Default: `Button's ['data-popup-target'] value`
+
+Popup to open (its `[data-popup-id]` value).
+
+### closeBtnSelector
+
+Type: `String`
+
+Default: `'.popup__close'`
+
+Popup's 'close' button selector.
+
+### openedClass
+
+Type: `String`
+
+Default: `'opened'`
+
+Class added to the popup when popup is opened.
+
+### openedBodyClass
+
+Type: `String`
+
+Default: `'popup-opened'`
+
+Class added to the body when popup is opened.
+
+### beforeOpen
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `popup`
+
+Example:
+```javascript
+$('.popupButton').popup({
+  beforeOpen: function (popup) {
+    console.log('Popup will open');
+  }
+});
+```
+
+Fires before popup will open.
+
+### afterOpen
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `popup`
+
+Fires when popup opened.
+
+### beforeClose
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `popup`
+
+Fires before popup will close.
+
+### afterClose
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `popup`
+
+Fires when popup closed.
+
+### remote
+
+Type: `Object|Boolean|String`
+
+Default: `Button's ['data-popup-remote'] value or false`
+
+Example:
+```javascript
+$('.popupButton').popup({
+  remote: {
+    url: 'ajax/request/path',
+    onComplete: function (XHR, textStatus) {
+      console.log('AJAX finished');
+    }
+  }
+});
+```
+
+Popup remote settings (or just AJAX request url).
+
+### remote.url
+
+Type: `String`
+
+Default: `n/a`
+
+AJAX url.
+
+### remote.data
+
+Type: `Any`
+
+Default: `n/a`
+
+AJAX data to send.
+
+### remote.onBeforeSend
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `[XHR, AJAXsettings]`
+
+AJAX 'beforeSend' callback.
+
+### remote.onComplete
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `[XHR, textStatus]`
+
+AJAX 'complete' callback.
+
+### remote.onError
+
+Type: `Function`
+
+Default: `n/a`
+
+Parameter: `[XHR, textStatus, errorThrown]`
+
+AJAX 'error' callback.
+
+## Methods
+
+### Non-static
+
+```javascript
+// Initialize first
+$('.popupButton').popup({
+  targetPopupId: 'examplePopup'
+});
+
+// Get access to popup's instance
+var popupInstance = $('[data-popup-id="examplePopup"]').data('popup');
+
+// Use methods
+
+// Open popup
+popupInstance.open();
+
+// With remote data
+popupInstance.open(ajaxResponse);
+
+// Close popup
+popupInstance.close();
+
+// Kill popup instance
+popupInstance.kill();
+```
+
+### Static
+
+```javascript
+/**
+ * Kill specified popup.
+ *
+ * @static
+ * @param {String|jQuery} popup
+ */
+Popup.kill(popup);
+
+/**
+ * Close all popups.
+ *
+ * @static
+ * @param {String} [openedClass='opened'] - opened popup class indicator
+ */
+Popup.closeAllPopup(openedClass);
+
+/**
+ * Initialize popup/popups with specified options.
+ *
+ * @static
+ * @param {String} [selector='[data-popup-target]']
+ * @param {Object} [options]
+ */
+Popup.initialize(selector, options);
+```
+
+## Requirement
+
+[jQuery 1.9.1+](http://jquery.com/)
+
+## Versioning
+
+Current version is 0.1.1
---
+++ src/vintage-popup.js
@@ -0,0 +1,464 @@
+;(function () {
+  /**
+   * Helpers.
+   */
+  var $window = $(window);
+  var $document = $(document);
+  var $body = $('body');
+  var $htmlbody = $('html, body');
+  var closeOnResizeFlag = false, closeOnEscFlag = false;
+  var isDevice = function () {
+    return window.matchMedia('(max-width: ' + 1199 + 'px)').matches;
+  };
+
+  /**
+   * Popup module.
+   *
+   * @param {jQuery} $button
+   * @param {Object} options
+   *
+   * @param {String} [options.openedClass='opened'] - Class added to the popup when it's open.
+   * @param {String} [options.openedBodyClass='popup-opened'] - Class added to the body when popup is open.
+   * @param {String} [options.closeBtnSelector='.popup__close'] - Popup's close button selector.
+   * @param {String} [options.targetPopupId='data-popup-target'] - Target popup (its data-popup-id value).
+   *
+   * @param {String} [options.eventsNameSpace='popup'] - jQuery events namespace.
+   * @param {Boolean} [options.closeOnBgClick=true] - If true, closes the popup by clicking anywhere outside it.
+   * @param {Boolean} [options.bodyFixedOnDesktop=true] - If true, sets position: fixed to the body on desktop.
+   * @param {Boolean} [options.closeOnEsc=true] - If true, closes the popup after pressing the ESC key.
+   *
+   * @param {Function} [options.beforeOpen]
+   * @param {Function} [options.afterOpen]
+   * @param {Function} [options.beforeClose]
+   * @param {Function} [options.afterClose]
+   *
+   * @param {Object|Boolean} [options.remote=false] - AJAX options
+   * @param {String} [options.remote.url]
+   * @param {Function} [options.remote.onBeforeSend]
+   * @param {Function} [options.remote.onError]
+   * @param {Function} [options.remote.onComplete]
+   * @param {*} [options.remote.data]
+   */
+  function Popup ($button, options) {
+    options = options || {};
+
+    this.options = {
+      openedClass: options.openedClass || 'opened',
+      openedBodyClass: options.openedBodyClass || 'popup-opened',
+      closeBtnSelector: options.closeBtnSelector || '.popup__close',
+      targetPopupId: options.targetPopupId || $button.data('popup-target'),
+
+      eventsNameSpace: options.eventsNameSpace || 'popup',
+      closeOnBgClick: typeof options.closeOnBgClick === 'boolean'
+        ? options.closeOnBgClick
+        : true,
+      bodyFixedOnDesktop: typeof options.bodyFixedOnDesktop === 'boolean'
+        ? options.bodyFixedOnDesktop
+        : true,
+      closeOnEsc: typeof options.closeOnEsc === 'boolean'
+        ? options.closeOnEsc
+        : true,
+
+      beforeOpen: options.beforeOpen,
+      afterOpen: options.afterOpen,
+      beforeClose: options.beforeClose,
+      afterClose: options.afterClose,
+
+      remote: options.remote || $button.data('popup-remote')
+    };
+
+    this.$button = $button;
+    this.$popup = $('[data-popup-id="' + this.options.targetPopupId + '"]');
+    this.defaultEvents =
+      'click.' + this.options.eventsNameSpace + ' tap.' + this.options.eventsNameSpace;
+    this.remoteData = this.options.remote
+      ? $.extend(true, this.options.remote, { url: this.options.remote.url || $button.data('popup-remote') })
+      : this.options.remote;
+
+    // initialize popup
+    this.activate();
+  }
+
+  /**
+   * Find and close all opened popups.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.checkAndCloseAllPopups = function () {
+    var $popups = $body.find('[data-popup-id]');
+    var $openedPopup = $popups.filter('.' + this.options.openedClass);
+
+    if ($openedPopup.length) {
+      var $openedPopupInstance = $openedPopup.data('popup');
+
+      this.prevPopupScrollTop = $openedPopupInstance.scrollTop;
+      $openedPopupInstance.close(true);
+    }
+
+    return this;
+  };
+
+  /**
+   * Check existence and run callback.
+   *
+   * @param {*} callback
+   * @returns {Popup}
+   */
+  Popup.prototype.checkAndRunCallback = function (callback) {
+    if (typeof callback === 'function') {
+      callback.call(null, this);
+    } else if (callback != undefined) {
+      console.warn('Callback should be a function.')
+    }
+
+    return this;
+  };
+
+  /**
+   * Open popup.
+   *
+   * @param {Object} remoteData - ajax request 'response' object
+   * @returns {Popup}
+   */
+  Popup.prototype.open = function (remoteData) {
+
+    // do stuff with remote data before popup open
+    if (remoteData) {
+      // actions with response
+      this.actionsWithRemoteData(remoteData);
+
+      // register 'close' button event
+      this.registerCloseBtnClick();
+    }
+
+    // before open callback
+    this.checkAndRunCallback(this.options.beforeOpen);
+
+    // add active class to body
+    if (this.options.bodyFixedOnDesktop || isDevice()) {
+      this.scrollTop = this.prevPopupScrollTop || $window.scrollTop();
+
+      // save scrollTop to data set
+      this.$popup.data('popupScrollTop', this.scrollTop);
+
+      $body
+        .css('top', -this.scrollTop)
+        .addClass(this.options.openedBodyClass);
+    }
+
+    // add active active class to popup
+    this.$popup.addClass(this.options.openedClass);
+
+    // after open callback
+    this.checkAndRunCallback(this.options.afterOpen);
+
+    return this;
+  };
+
+  /**
+   * Close popup.
+   *
+   * @param {Boolean} hasOpenedPopups - close popup without changing body styles
+   * @returns {Popup}
+   */
+  Popup.prototype.close = function (hasOpenedPopups) {
+    // before close callback
+    this.checkAndRunCallback(this.options.beforeClose);
+
+    // remove active class from body
+    if (!hasOpenedPopups) {
+      $body
+        .removeAttr('style')
+        .removeClass(this.options.openedBodyClass);
+
+      $htmlbody
+        .scrollTop(this.$popup.data('popupScrollTop'));
+
+      this.prevScrollTop ? this.prevScrollTop = false : null;
+    }
+
+    // remove active class from popup
+    this.$popup.removeClass(this.options.openedClass);
+
+    // after close callback
+    this.checkAndRunCallback(this.options.afterClose);
+
+    return this;
+  };
+
+  /**
+   * Actions with remote data.
+   *
+   * @param {Object} remoteData
+   * @returns {Popup}
+   */
+  Popup.prototype.actionsWithRemoteData = function (remoteData) {
+    if (remoteData.replaces instanceof Array) {
+      for (var i = 0, ilen = remoteData.replaces.length; i < ilen; i++) {
+        $(remoteData.replaces[i].what).replaceWith(remoteData.replaces[i].data);
+      }
+    }
+    if (remoteData.append instanceof Array) {
+      for (i = 0, ilen = remoteData.append.length; i < ilen; i++) {
+        $(remoteData.append[i].what).append(remoteData.append[i].data);
+      }
+    }
+    if (remoteData.content instanceof Array) {
+      for (i = 0, ilen = remoteData.content.length; i < ilen; i++) {
+        $(remoteData.content[i].what).html(remoteData.content[i].data);
+      }
+    }
+    if (remoteData.js) {
+      $body.append(remoteData.js);
+    }
+    if (remoteData.refresh) {
+      window.location.reload(true);
+    }
+    if (remoteData.redirect) {
+      window.location.href = remoteData.redirect;
+    }
+  };
+
+  /**
+   * Open popup on button's click.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.registerOpenOnClick = function () {
+    var _this = this;
+
+    this.$button.unbind(this.defaultEvents).on(this.defaultEvents, function (e) {
+      e.preventDefault();
+
+      // find opened popups and close them
+      _this.checkAndCloseAllPopups();
+
+      // REMOTE DATA
+      if (_this.remoteData) {
+        var remote = _this.remoteData;
+
+        $.ajax({
+          cache: 'false',
+          method: 'get',
+          dataType: 'json',
+          url: remote.url,
+          data: remote.data,
+          beforeSend: remote.onBeforeSend,
+          complete: remote.onComplete,
+          success: _this.open.bind(_this),
+          error: remote.onError
+        });
+
+        return this;
+      }
+
+      // open popup
+      _this.open(false);
+    });
+
+    return this;
+  };
+
+  /**
+   * Close popup on window's resize.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.registerCloseOnResize = function () {
+    var _this = this;
+    var events = 'resize.' + this.options.eventsNameSpace;
+
+    if (!closeOnResizeFlag) {
+      $window.on(events, function () {
+        if (!isDevice() && $body.hasClass(_this.options.openedBodyClass)) {
+          Popup.closeAllPopups(_this.options.openedClass);
+        }
+      });
+
+      closeOnResizeFlag = true;
+    }
+
+    return this;
+  };
+
+  /**
+   * Close popup on esc button click.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.registerCloseOnEsc = function () {
+    var _this = this;
+    var events = 'keyup.' + this.options.eventsNameSpace;
+
+    if (!closeOnEscFlag) {
+      $document.on(events, function (event) {
+        if (event.keyCode == 27 && $body.hasClass(_this.options.openedBodyClass)) {
+          Popup.closeAllPopups(_this.options.openedClass);
+        }
+      });
+
+      closeOnEscFlag = true;
+    }
+
+    return this;
+  };
+
+  /**
+   * Close popup on modal's background click.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.registerCloseOnBgClick = function () {
+    var _this = this;
+
+    _this.$popup.on(this.defaultEvents, function (event) {
+      if (event.target === _this.$popup.get(0)) _this.close();
+    });
+
+    return this;
+  };
+
+  /**
+   * Close popup on close button click.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.registerCloseBtnClick = function () {
+    var _this = this;
+    var $closeButton = _this.$popup.find(this.options.closeBtnSelector);
+
+    if (!$closeButton.unbind || !$closeButton.on) {
+      console.warn('Close button was not found');
+
+      return this;
+    }
+
+    $closeButton.unbind(this.defaultEvents).on(this.defaultEvents, function () {
+      _this.close();
+    });
+
+    return this;
+  };
+
+  /**
+   * Register all events.
+   *
+   * @returns {Popup}
+   */
+  Popup.prototype.activate = function () {
+
+    // if popup was already activated
+    if (this.$popup.data('popup')) {
+      this.registerOpenOnClick();
+      this.$popup.data('popup', this);
+
+      return this;
+    }
+
+    // 'close' button
+    this.registerCloseBtnClick();
+
+    // save Popup instance data
+    this.$popup.data('popup', this);
+
+    // close popup on 'Esc' click
+    if (this.options.closeOnEsc) this.registerCloseOnEsc();
+
+    // close popup when clicked anywhere on the black background
+    if (this.options.closeOnBgClick) this.registerCloseOnBgClick();
+
+    // open popup on click (button), close on resize
+    this
+      .registerOpenOnClick()
+      .registerCloseOnResize();
+
+    return this;
+  };
+
+  /**
+   * Removes event listener from button and destroys associated data.
+   */
+  Popup.prototype.kill = function () {
+    this.$button.unbind(this.defaultEvents);
+    this.$popup.data('popup', null);
+  };
+
+  /**
+   * Kill specified popup.
+   *
+   * @static
+   * @param {String|jQuery} popup
+   */
+  Popup.kill = function (popup) {
+    var popupInstance = $(popup).data('popup');
+
+    popupInstance.kill();
+  };
+
+  /**
+   * Close all popups.
+   *
+   * @static
+   * @param {String} [openedClass='opened'] - css class-indicator
+   */
+  Popup.closeAllPopups = function (openedClass) {
+    openedClass = openedClass || 'opened';
+    var $popups = $body.find('[data-popup-id]');
+    var $openedPopup = $popups.filter('.' + openedClass);
+
+    if ($openedPopup.length) {
+      var $openedPopupInstance = $openedPopup.data('popup');
+
+      $openedPopupInstance.close.call($openedPopupInstance, false);
+    }
+  };
+
+  /**
+   * Initialize all popups.
+   *
+   * @static
+   * @param {String} [selector='[data-popup-target]']
+   * @param {Object} [options]
+   * @returns {jQuery}
+   */
+  Popup.initialize = function (selector, options) {
+    var defaultSelector = '[data-popup-target]';
+
+    selector = selector || defaultSelector;
+
+    var isJQuery = selector instanceof jQuery;
+    var isString = typeof selector === 'string';
+    var isObject = typeof selector === 'object';
+
+    if (!isString && !isJQuery && isObject) {
+      options = selector;
+      selector = defaultSelector;
+    }
+
+    return $(selector).popup(options);
+  };
+
+  /**
+   * Expose Popup module.
+   */
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    // CommonJS, just export
+    module.exports = Popup;
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD support
+    define('vintage-popup', function () { return Popup; });
+  } else {
+    // Global
+    window.Popup = Popup;
+  }
+
+  /**
+   * Expose Popup module.
+   */
+  $.fn.popup = function (options) {
+    return this.each(function () {
+      var $this = $(this);
+      new Popup($this, options);
+    });
+  };
+})();
